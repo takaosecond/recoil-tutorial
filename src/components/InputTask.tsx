516a0e018af2c78e0b23f794821ddfa4770bd1ac
takaosecond <takaosecond@icloud.com>
2022-09-23 20:08:18 +0900

    first commit

---
+++ src/components/InputTask.tsx
@@ -0,0 +1,42 @@
+import React, { useCallback } from "react";
+import { useRecoilState } from "recoil";
+import { addTitleState } from "../states/addTitleState";
+import { inputTitleState } from "../states/inputTitleState";
+import "./InputTask.css";
+
+const getKey = () => Math.random().toString(32).substring(2);
+
+const InputTask = () => {
+  const [inputTitle, setInputTitle] = useRecoilState(inputTitleState);
+  const [addTitle, setAddTitle] = useRecoilState(addTitleState);
+
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setInputTitle(e.target.value);
+      console.log(inputTitle);
+    },
+    [inputTitle]
+  );
+
+  const handleClick = () => {
+    setAddTitle([...addTitle, { id: getKey(), title: inputTitle }]);
+    setInputTitle("");
+    console.log(addTitle);
+  };
+
+  return (
+    <div className="inputField">
+      <input
+        type="text"
+        className="inputTitle"
+        onChange={onChange}
+        value={inputTitle}
+      />
+      <button type="button" className="addButton" onClick={handleClick}>
+        追加
+      </button>
+    </div>
+  );
+};
+
+export default InputTask;
